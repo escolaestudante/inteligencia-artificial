3c55170d03f00f968b2ea2e29bf15d6f1199e13c
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-08-13 08:15:03 -0300

    DE

--- script.js
+++ script.js
@@ -83,3 +83,14 @@ const perguntas = [
         ],
     },
 ];
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", function () {
+        atual++;
+        mostraPergunta();
+      });
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
