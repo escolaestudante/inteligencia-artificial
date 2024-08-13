b844c92eed5af642900dac710b98f20fc8568267
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-08-13 08:25:38 -0300

    RGTRT

--- script.js
+++ script.js
@@ -83,4 +83,21 @@ const perguntas = [
         ],
     },
 ];
-
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", function () {
+        atual++;
+        mostraPergunta();
+      });
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+  function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
+mostraPergunta();
