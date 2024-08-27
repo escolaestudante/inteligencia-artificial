70133a6ab46755b3ffcded136f5637d67bd3edb5
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-08-27 07:34:56 -0300

    sf

--- script.js
+++ script.js
@@ -89,24 +89,37 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
-        if (atual >= perguntas.length) {
-            mostraResultado();
-            return;
-        }
-        perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado;
-        caixaAlternativa.textContent
-        mostraAlternativas();
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
-    D
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function () {
-            atual++;
-            mostraPergunta();
-        });
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
+
